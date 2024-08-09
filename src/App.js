28bd1c39b7339384f7158412ffda477ac2a4e907
Durgasuresh4303 <durgasuresh1026@gmail.com>
2024-08-09 15:08:56 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo1 from './components/Classcomp';
+import Demo from './components/Funccomp';
+
+function App(){
+return (
+  <div className="App">
+
+    <h2>Hello everyone This is my application </h2>
+    <Demo1></Demo1>
+    <Demo></Demo>
+  </div>
+);
+}
+
+
